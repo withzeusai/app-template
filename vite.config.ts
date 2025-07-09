67998e12876ce06b53acac9752ddaf3fd9478d80
Grant Gurvis <grantgurvis@gmail.com>
2025-07-08 19:36:30 -0700

    chore: clean up deps file

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+    },
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
         },
     },
-    server: {
-        host: true,
-    }
 })
