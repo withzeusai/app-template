876238d3ba1573f88d44862af68f81ed925112c1
parssak <parssak@gmail.com>
2025-08-04 13:05:41 -0400

    design mode wip

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,16 @@
+const Bongo = () => {
+  return <h1 className="bg-red-500">Bongo</h1>;
+};
+
 export default function Index() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-background">
       <div className="text-center space-y-6">
-        <h1 className="text-4xl text-balance font-bold tracking-tight">
+        <h1 className="text-9xl text-balance font-bold tracking-tight">
           Welcome to Your Blank App
         </h1>
-        <p className="text-xl text-muted-foreground max-w-2xl mx-auto">
-          Start chatting to build your app
-        </p>
+
+        <Bongo />
       </div>
     </div>
   );
