c07d265e91f5853e7fb72132286f37e3b862c2c9
Grant Gurvis <grantgurvis@gmail.com>
2025-08-10 17:37:50 -0700

    fix: use root as redirect

--- src/components/providers/auth.tsx
+++ src/components/providers/auth.tsx
@@ -7,7 +7,7 @@ import {
 const AUTH_CONFIG: AuthProviderProps = {
   authority: import.meta.env.VITE_HERCULES_OIDC_AUTHORITY!,
   client_id: import.meta.env.VITE_HERCULES_OIDC_CLIENT_ID!,
-  redirect_uri: import.meta.env.VITE_HERCULES_OIDC_REDIRECT_URI!,
+  redirect_uri: window.location.origin,
   response_type: "code",
   scope: "openid profile email",
 };
