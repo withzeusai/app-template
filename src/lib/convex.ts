a370c979a35acad41061be4ccaae75c20f2f4e27
Grant Gurvis <grantgurvis@gmail.com>
2025-08-14 20:06:41 -0700

    feat: convex cleanup

--- src/lib/convex.ts
+++ src/lib/convex.ts
@@ -1,4 +1,6 @@
 import { ConvexReactClient } from "convex/react";
 
+export { api } from "../../convex/_generated/api";
+
 export const convexUrl = import.meta.env.VITE_CONVEX_URL;
 export const convex = new ConvexReactClient(convexUrl);
