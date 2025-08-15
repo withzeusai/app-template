9ee0358cb90c75501c0a51750fc49b684e975f8d
Grant Gurvis <grantgurvis@gmail.com>
2025-08-14 21:59:44 -0700

    eslint: add generated to ignore

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config([
-  globalIgnores(["dist"]),
+  globalIgnores(["dist", "**/_generated/*"]),
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
