c7606a78a30a6a1cb917ab876497e7318c903352
Grant Gurvis <grantgurvis@gmail.com>
2025-08-18 13:47:54 -0700

    feat: add @usehercules/eslint-plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import convexPlugin from "@convex-dev/eslint-plugin";
 import js from "@eslint/js";
+import herculesPlugin from "@usehercules/eslint-plugin";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import { globalIgnores } from "eslint/config";
@@ -16,6 +17,7 @@ export default tseslint.config([
       reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
       convexPlugin.configs.recommended,
+      herculesPlugin.configs.recommended,
     ],
     rules: {
       "react-refresh/only-export-components": [
