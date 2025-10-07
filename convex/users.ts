91db023d85e064ff3c36978697eaae2ab4505e33
Grant Gurvis <grantgurvis@gmail.com>
2025-10-06 19:32:57 -0700

    fix: improve error handling for unauthenticated user in storeUser mutation

--- convex/users.ts
+++ convex/users.ts
@@ -1,3 +1,5 @@
+import { ConvexError } from "convex/values";
+
 import { mutation } from "./_generated/server";
 
 export const store = mutation({
@@ -5,7 +7,10 @@ export const store = mutation({
   handler: async (ctx) => {
     const identity = await ctx.auth.getUserIdentity();
     if (!identity) {
-      throw new Error("Called storeUser without authentication present");
+      throw new ConvexError({
+        code: "UNAUTHENTICATED",
+        message: "Called storeUser without authentication present",
+      });
     }
 
     // Check if we've already stored this identity before.
@@ -16,13 +21,6 @@ export const store = mutation({
       )
       .unique();
     if (user !== null) {
-      // If we've seen this identity before but the name or email has changed, patch the value.
-      if (user.name !== identity.name || user.email !== identity.email) {
-        await ctx.db.patch(user._id, {
-          name: identity.name,
-          email: identity.email,
-        });
-      }
       return user._id;
     }
     // If it's a new identity, create a new User.
