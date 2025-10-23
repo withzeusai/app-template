01c0f6f684b8592f2ee99d39d20f29d0f7de037a
Grant Gurvis <grantgurvis@gmail.com>
2025-10-22 20:27:32 -0700

    feat: use @usehercules/auth package

--- src/components/providers/auth.tsx
+++ src/components/providers/auth.tsx
@@ -1,35 +1,17 @@
-import { useCallback } from "react";
-import {
-  AuthProvider as ReactAuthProvider,
-  type AuthProviderProps,
-} from "react-oidc-context";
+import { type AuthProviderProps } from "react-oidc-context";
+import { HerculesAuthProvider } from "@usehercules/auth/react";
 
 const AUTH_CONFIG: AuthProviderProps = {
-  authority: import.meta.env.VITE_HERCULES_OIDC_AUTHORITY!,
-  client_id: import.meta.env.VITE_HERCULES_OIDC_CLIENT_ID!,
-  prompt: import.meta.env.VITE_HERCULES_OIDC_PROMPT ?? "select_account",
-  response_type: import.meta.env.VITE_HERCULES_OIDC_RESPONSE_TYPE ?? "code",
-  scope: import.meta.env.VITE_HERCULES_OIDC_SCOPE ?? "openid profile email",
-  redirect_uri:
-    import.meta.env.VITE_HERCULES_OIDC_REDIRECT_URI ??
-    `${window.location.origin}/auth/callback`,
+  authority: import.meta.env.VITE_HERCULES_OIDC_AUTHORITY ?? "",
+  client_id: import.meta.env.VITE_HERCULES_OIDC_CLIENT_ID ?? "",
+  prompt: import.meta.env.VITE_HERCULES_OIDC_PROMPT,
+  response_type: import.meta.env.VITE_HERCULES_OIDC_RESPONSE_TYPE,
+  scope: import.meta.env.VITE_HERCULES_OIDC_SCOPE,
+  redirect_uri: import.meta.env.VITE_HERCULES_OIDC_REDIRECT_URI,
 };
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-  const onSigninCallback = useCallback(() => {
-    window.history.replaceState({}, document.title, window.location.pathname);
-  }, []);
-  const onSignoutCallback = useCallback(() => {
-    window.location.pathname = "";
-  }, []);
-
   return (
-    <ReactAuthProvider
-      {...AUTH_CONFIG}
-      onSigninCallback={onSigninCallback}
-      onSignoutCallback={onSignoutCallback}
-    >
-      {children}
-    </ReactAuthProvider>
+    <HerculesAuthProvider config={AUTH_CONFIG}>{children}</HerculesAuthProvider>
   );
 }
--- src/components/providers/default.tsx
+++ src/components/providers/default.tsx
@@ -1,17 +1,15 @@
-import { ConvexProviderWithAuth } from "convex/react";
 import { ThemeProvider } from "next-themes";
-import { convex } from "@/lib/convex.ts";
-import { useAuth } from "@/hooks/use-auth.ts";
 import { Toaster } from "@/components/ui/sonner.tsx";
 import { TooltipProvider } from "@/components/ui/tooltip.tsx";
 import { AuthProvider } from "@/components/providers/auth.tsx";
 import { QueryClientProvider } from "@/components/providers/query-client.tsx";
 import { UpdateCurrentUserProvider } from "@/components/providers/update-current-user.tsx";
+import { ConvexProvider } from "./convex";
 
 export function DefaultProviders({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
-      <ConvexProviderWithAuth client={convex} useAuth={useAuth}>
+      <ConvexProvider>
         <QueryClientProvider>
           <UpdateCurrentUserProvider>
             <TooltipProvider>
@@ -26,7 +24,7 @@ export function DefaultProviders({ children }: { children: React.ReactNode }) {
             </TooltipProvider>
           </UpdateCurrentUserProvider>
         </QueryClientProvider>
-      </ConvexProviderWithAuth>
+      </ConvexProvider>
     </AuthProvider>
   );
 }
