2e245610a795ad5ed62adb5d3bdb2270b68886ed
parssak <parssak@gmail.com>
2025-11-05 09:33:02 -0800

    better skeleton

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -4,7 +4,10 @@ function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="skeleton"
-      className={cn("bg-muted-foreground/10 animate-pulse rounded-md", className)}
+      className={cn(
+        "bg-muted-foreground/10 rounded-md opacity-0 animate-[fade-in_1s_ease-in-out_1s_forwards,pulse_2s_ease-in-out_1s_infinite]",
+        className
+      )}
       {...props}
     />
   );
